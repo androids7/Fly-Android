d8d0d007d1ea04f78ac32777387dd99cb834ab21
fangcun <fangcun010@gmail.com>
2018-04-22 17:16:44 +0800

    Update

--- Fly/app/src/main/cpp/NativeMain.cpp
+++ Fly/app/src/main/cpp/NativeMain.cpp
@@ -1,4 +1,7 @@
 #include <android/log.h>
+#include <stdlib.h>
+#include <EGL/egl.h>
+#include <GLES/gl.h>
 #include <pthread.h>
 
 #include "NativeMain.h"
@@ -9,6 +12,11 @@
 
 static bool isLoop=false;
 static pthread_t loopID;
+static pthread_mutex_t mut;
+
+Vector *pEventVt;
+EGLDisplay display;
+EGLSurface surface;
 
 void onStart(ANativeActivity *activity) {
     LOGE("Application is start");
@@ -41,8 +49,67 @@ void onWindowFocusChanged(ANativeActivity *activity, int hasFocus) {
 
 }
 
-void onNativeWindowCreated(ANativeActivity *activity, ANativeWindow *window) {
+ANativeWindow *gWindow;
 
+void InitDisplay(ANativeWindow *window){
+
+    const EGLint attribs[] = {
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_BLUE_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_RED_SIZE, 8,
+            EGL_NONE
+    };
+    EGLint w, h, dummy, format;
+    EGLint numConfigs;
+    EGLConfig config;
+    EGLSurface surface;
+    EGLContext context;
+
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+
+    int ver;
+    eglInitialize(display, &ver, 0);
+
+
+    /* Here, the application chooses the configuration it desires. In this
+     * sample, we have a very simplified selection process, where we pick
+     * the first EGLConfig that matches our criteria */
+    eglChooseConfig(display, attribs, &config, 1, &numConfigs);
+
+    /* EGL_NATIVE_VISUAL_ID is an attribute of the EGLConfig that is
+     * guaranteed to be accepted by ANativeWindow_setBuffersGeometry().
+     * As soon as we picked a EGLConfig, we can safely reconfigure the
+     * ANativeWindow buffers to match, using EGL_NATIVE_VISUAL_ID. */
+    eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
+
+
+
+    ANativeWindow_setBuffersGeometry(window, 0, 0, format);
+
+    surface = eglCreateWindowSurface(display, config, window, NULL);
+    context = eglCreateContext(display, config, NULL, NULL);
+
+    if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
+        LOGE("Unable to eglMakeCurrent");
+        return;
+    }
+
+    eglQuerySurface(display, surface, EGL_WIDTH, &w);
+    eglQuerySurface(display, surface, EGL_HEIGHT, &h);
+
+    // Initialize GL state.
+    ::display=display;
+    ::surface=surface;
+}
+
+void onNativeWindowCreated(ANativeActivity *activity, ANativeWindow *window) {
+    gWindow=window;
+    Event *pEvent=(Event *)malloc(sizeof(Event));
+    pEvent->EventID=1111;
+    pthread_mutex_lock(&mut);
+    vtAddBack(pEventVt,pEvent);
+    pthread_mutex_unlock(&mut);
 }
 
 void onNativeWindowDestroyed(ANativeActivity *activity, ANativeWindow *window) {
@@ -58,11 +125,25 @@ void onInputQueueCreated(ANativeActivity *activity, AInputQueue *queue) {
 void onInputQueueDestroyed(ANativeActivity *activity, AInputQueue *queue) {
     isLoop=false;
 }
+
+void DoEvent(){
+    InitDisplay(gWindow);
+    glClearColor(1,0,0,1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    eglSwapBuffers(display,surface);
+}
+
 void *looper(void *args) {
     ANativeActivity *activity = (ANativeActivity *) args;
     AInputQueue *queue = (AInputQueue *) activity->instance;
     AInputEvent *event = NULL;
     while (isLoop) {
+        pthread_mutex_lock(&mut);
+        if(vtCount(pEventVt)){
+            DoEvent();
+        }
+        pthread_mutex_unlock(&mut);
+
         if (!AInputQueue_hasEvents(queue)) {
             continue;
         }
@@ -73,7 +154,6 @@ void *looper(void *args) {
             case AINPUT_EVENT_TYPE_MOTION: {
                 switch (AMotionEvent_getAction(event)) {
                     case AMOTION_EVENT_ACTION_DOWN: {
-                        LOGE("Touch Scerrn Down");
                         break;
                     }
                     case AMOTION_EVENT_ACTION_UP: {
@@ -114,6 +194,9 @@ void onLowMemory(ANativeActivity *activity) {
 }
 
 void ANativeActivity_onCreate(ANativeActivity *activity, void *savedState, size_t savedStateSize) {
+    pEventVt=vtCreate();
+    LOGE("SSSSSS");
+    pthread_mutex_init(&mut,NULL);
     bindLifeCycle(activity);
 }
 
